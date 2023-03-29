b16d2003456f2384ec04d4e122652fcff3567b90
Gerard Navarro <navarro.gerard@gmail.com>
2023-03-30 00:51:29 +0800

    Initial Commit

---
+++ GLFW OpenGL/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  GLFW OpenGL
+//
+//  Created by Navarro on 3/30/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
